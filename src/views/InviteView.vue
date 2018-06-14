5b72820d96b2b8e20898f14a4dc9eb00b91a56f6
Frank Wei <frank@frankwei.xyz>
2018-06-14 15:13:18 +0800

    fix: can get refer address now

--- src/views/InviteView.vue
+++ src/views/InviteView.vue
@@ -2,7 +2,7 @@
     <div class="back_img">
         <div class="title11">
          <div class="line1"> {{title}}</div>
-    </div> 
+    </div>
         <div class="back_color">
             <div class="title_1">
                 <div class="title_2"><b>推荐好友玩 星云水浒 可获得 3 % 消费额的奖励!</b></div>
@@ -10,7 +10,7 @@
             <div class="input1">
             <div class="title is-5 title3">你的邀请链接: </div>
             <div class="input">
-                <input type="search"  value="请解锁 MetaMask 钱包再刷新访问"> 
+                <input type="search" :value="myRefferalLink" disabled>
             </div>
                 <div class="btn">复制链接</div>
             </div>
@@ -22,7 +22,8 @@
                 </div>
                 <div class="app_list">
                     <ul>
-                    <li v-for="n in 8" :key="n.id" ><img v-bind:src="'/static/assets/invite/'+index+'.png'"> </li>
+                    <li v-for="n in 8" :key="n.id" >
+                        <img v-bind:src="'/static/assets/invite/'+index+'.png'"> </li>
                     </ul>
                 </div>
             </div>
@@ -30,25 +31,41 @@
     </div>
 </template>
 <script>
-export default {
-    name: 'InviteView',
-    data(){
-   return{
-        title:"邀请好友",
-methods :{
-         Invite: function(index){
-             return "Invite_"+ index
-         }
-     },
+import Clipboard from 'clipboard';
+import { mapState } from 'vuex';
 
-        }
-    }
+export default {
+  created() {
+    const clipboard = new Clipboard('.button');
+    clipboard.on('success', (e) => {
+      e.clearSelection();
+    });
+  },
+  name: 'InviteView',
+  data: () => ({
+    title: '邀请好友',
+  }),
+  computed: {
+    ...mapState(['me']),
+    myRefferalLink() {
+      const website = 'https://cryptohero-nas.etherfen.com/#';
+      if (this.me) {
+        return `${website}/draw/${this.me}`;
+      }
+      return '请安装钱包插件再来';
+    },
+  },
+  methods: {
+    invite(index) {
+      return `Invite_${index}`;
+    },
+  },
 };
 </script>
 
 <style scoped>
 .back_img{
-    background: url(/static/assets/card_profile_top.png) no-repeat top , 
+    background: url(/static/assets/card_profile_top.png) no-repeat top ,
      url(/static/assets/card_profile_end.png) no-repeat bottom,
      url(/static/assets/card_profile.png) repeat-y ;
     background-size: 100%;
@@ -63,7 +80,7 @@ methods :{
      width: 100%;
      height: 50px;
      display: flex;
-    justify-content: center 
+    justify-content: center
 }
 .line1{
      width: 30%;
@@ -115,10 +132,10 @@ methods :{
 }
 input{
     width: 100%;
-    background:none;    
-    outline:none;    
+    background:none;
+    outline:none;
     border:0px;
-    color: #906718;  
+    color: #906718;
 }
 .invite{
     width: 100%;
