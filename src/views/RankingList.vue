93d8cf6fc5f53ebc5076167a2573fea7f8d1793e
fqh <fqhua_5@sina.cn>
2018-06-23 00:17:29 +0800

    排行榜排序

--- src/views/RankingList.vue
+++ src/views/RankingList.vue
@@ -43,8 +43,28 @@ import Contract from '../contract/cryptohero';
     async mounted() {
       const  contract = new Contract();
       const  res = await contract.getHoldersStat();
-      this.items = res;
-      console.log(res);
+      if(res !== null){
+        this.items = res.sort(this.compare('balance'));
+      }
+    },
+    methods: {
+      compare(prop) {
+        return function (obj1, obj2) {
+          var val1 = obj1[prop];
+          var val2 = obj2[prop];
+          if (!isNaN(Number(val1)) && !isNaN(Number(val2))) {
+            val1 = Number(val1);
+            val2 = Number(val2);
+          }
+          if (val1 < val2) {
+            return 1;
+          } else if (val1 > val2) {
+            return -1;
+          } else {
+            return 0;
+          }
+        }
+      }
     }
   }
 
