8dd3b5c02f8f7b6fd65f395bda99947b4e8fe9d2
gloria12345678 <quyi28266687@gmail.com>
2018-06-25 00:16:43 +0800

    airdrop.ui

--- src/views/AirDrop.vue
+++ src/views/AirDrop.vue
@@ -12,20 +12,20 @@
                       h2.subtitle| {{$t('H2Title3')}}
                       h1.title| {{ getPrice }} NAS / {{$t('CardUnit')}}
             .container
-                input(v-model="to" placeholder="你要送给的地址")
+                h4.text|赠送卡牌
+                  input(v-model="to" placeholder="你要送给的地址")
                 .buttons(style="width: 18rem")
-                  a.button.is-primary(@click="setQty(1)")|{{$t('Draw')}} 1 {{$t('CardUnit')}}
-                  a.button.is-primary(@click="setQty(3)")|{{$t('Draw')}} 3 {{$t('CardUnit')}}
-                  a.button.is-primary(@click="setQty(6)")|{{$t('Draw')}} 6 {{$t('CardUnit')}}
-                  a.button.is-primary(@click="setQty(9)")|{{$t('Draw')}} 9 {{$t('CardUnit')}}
-                  a.button.is-primary(@click="setQty(12)")|{{$t('Draw')}} 12 {{$t('CardUnit')}}
-                  a.button.is-primary(@click="setQty(16)")|{{$t('Draw')}} 16 {{$t('CardUnit')}}
-                  a.button.is-primary(@click="setQty(32)")|{{$t('Draw')}} 32 {{$t('CardUnit')}}
-                  a.button.is-primary(@click="setQty(64)")|{{$t('Draw')}} 64 {{$t('CardUnit')}}
-                  a.button.is-primary(@click="setQty(128)")|{{$t('Draw')}} 128 {{$t('CardUnit')}}
-                  a.button.is-primary(@click="setQty(1024)")|{{$t('Draw')}} 1024 {{$t('CardUnit')}}
-                  <br>
-                   a.button.is-primary(@click="airdrop")|{{$t('赠送')}}
+                  a.button.is-primary(@click="setQty(1)")|{{$t('Gift')}} 1 {{$t('CardUnit')}}
+                  a.button.is-primary(@click="setQty(3)")|{{$t('Gift')}} 3 {{$t('CardUnit')}}
+                  a.button.is-primary(@click="setQty(6)")|{{$t('Gift')}} 6 {{$t('CardUnit')}}
+                  a.button.is-primary(@click="setQty(9)")|{{$t('Gift')}} 9 {{$t('CardUnit')}}
+                  a.button.is-primary(@click="setQty(12)")|{{$t('Gift')}} 12 {{$t('CardUnit')}}
+                  a.button.is-primary(@click="setQty(16)")|{{$t('Gift')}} 16 {{$t('CardUnit')}}
+                  a.button.is-primary(@click="setQty(32)")|{{$t('Gift')}} 32 {{$t('CardUnit')}}
+                  a.button.is-primary(@click="setQty(64)")|{{$t('Gift')}} 64 {{$t('CardUnit')}}
+                  a.button.is-primary(@click="setQty(128)")|{{$t('Gift')}} 128 {{$t('CardUnit')}}
+                  a.button.is-primary(@click="setQty(1024)")|{{$t('Gift')}} 1024 {{$t('CardUnit')}}
+                  //  a.button.is-primary(@click="airdrop")|{{$t('赠送')}}
 
             //- .container
               .columns
@@ -101,44 +101,44 @@ export default {
         this.count -= time;
       }
     },
-    async draw() {
+    async airdrop() {
       const contract = new Contract();
       const referrer = Cookie.get('referrer') || '';
 
       // console.log("crytpresp:"+referrer);
-      const result = await contract.draw(this.to, referrer);
+      const result = await contract.airdrop(this.to, referrer);
       // console.log("crytpresp00:"+result);
 
-      if (result != 'cancel') {
-        setTimeout(async () => {
-          const result1 = await contract.checkSerialNumber(result);
-          if (JSON.parse(result1).data.status == 1) {
-            if (referrer) {
-              const formData = new FormData();
-              formData.append('address', this.$store.state.me);
-              // formData.append('address', referrer);
-              formData.append('inviteaddress', referrer); // this.$route.params.address);
-              formData.append('cardnum', this.count);
-              formData.append('price', this.getPrice);
-              formData.append('witchnet', this.$store.getters.getContractNet); // "test");
-              formData.append('sn', result);
-              this.$http
-                .post(
-                  `${this.$store.getters.getServerURL}inviteshuihuadd.php`,
-                  formData,
-                )
-                .then((response) => {
-                  const res = response.body;
-                  console.log(res);
-                  alert('抽卡成功，到我的收藏里看看吧');
-                });
-            } else {
-              alert('抽卡成功，到我的收藏里看看吧');
-            }
-          }
-          // console.log("crytpresp:"+JSON.parse(result1)["msg"]);
-        }, 20000);
-      }
+      // if (result != 'cancel') {
+      //   setTimeout(async () => {
+      //     const result1 = await contract.checkSerialNumber(result);
+      //     if (JSON.parse(result1).data.status == 1) {
+      //       if (referrer) {
+      //         const formData = new FormData();
+      //         formData.append('address', this.$store.state.me);
+      //         // formData.append('address', referrer);
+      //         formData.append('inviteaddress', referrer); // this.$route.params.address);
+      //         formData.append('cardnum', this.count);
+      //         formData.append('price', this.getPrice);
+      //         formData.append('witchnet', this.$store.getters.getContractNet); // "test");
+      //         formData.append('sn', result);
+      //         this.$http
+      //           .post(
+      //             `${this.$store.getters.getServerURL}inviteshuihuadd.php`,
+      //             formData,
+      //           )
+      //           .then((response) => {
+      //             const res = response.body;
+      //             console.log(res);
+      //             alert('抽卡成功，到我的收藏里看看吧');
+      //           });
+      //       } else {
+      //         alert('抽卡成功，到我的收藏里看看吧');
+      //       }
+      //     }
+      //     // console.log("crytpresp:"+JSON.parse(result1)["msg"]);
+      //   }, 20000);
+      // }
     },
 
     async airdrop() {
@@ -194,5 +194,16 @@ export default {
 }
 .buttons {
   margin: 1rem;
+}
+h4{
+  padding-left: 17px;
+  }
+input{
+     width: 400px;
+    height: 35px;
+    background-color: tan;
+    outline: none;
+    border: 2px solid #fff8df;
+    border-radius: 5px;
 }
 </style>
